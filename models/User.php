b8437049b0d1a22b94e141bbbe7188d0dbab2c16
s <xiangyu@czxy.com>
2018-09-03 17:16:11 +0800

    邮箱

--- models/User.php
+++ models/User.php
@@ -1,12 +1,18 @@
 <?php 
 namespace models;
+use PDO;
 
     class User
     {
-        public function getName()
+        public $pdo;
+        public function __construct()
         {
-            
-            return 'Tom';
+            $this->pdo = new PDO('mysql:host=localhost;dbname=blog001','root','');
+            $this->pdo->exec('set names utf8');
+        }
+        public function add($email,$password){
+            $stmt = $this->pdo->prepare("INSERT INTO users(email,password) VALUES(?,?)");
+            return $stmt->execute([$email,$password]);
         }
     }
 ?>
